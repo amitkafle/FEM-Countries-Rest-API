0d0165aa2abf70a0ddab0ea4bd4ee3012e5b8867
Amit Kafle <amitkafleofficial@gmail.com>
2021-12-06 21:14:02 +0545

    feat: add header

---
+++ src/components/ui/Header.js
@@ -0,0 +1,20 @@
+import Container from "./Container"
+import { Link } from "react-router-dom"
+import Theme from "./Theme"
+
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <Container className={'header__container'}>
+                <Link to='/home'>
+                    <h1 className='header__title'>Where in the world?</h1>
+                </Link>
+                <Theme />
+            </Container>
+        </header>
+    )
+}
+
+export default Header
